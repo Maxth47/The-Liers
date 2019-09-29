5193eea4eb011336e6aee53a913a56dbceb6e7a2
Thanh Le <nhatthanhh1993@gmail.com>
2019-09-29 12:02:16 +0300

    create 3 fragments, design Fragment_Playing, write/read history file in External storage

--- app/src/main/java/com/example/theliers/MainActivity.kt
+++ app/src/main/java/com/example/theliers/MainActivity.kt
@@ -1,47 +1,28 @@
 package com.example.theliers
 
-import android.content.Context
-import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import com.squareup.seismic.ShakeDetector
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
+class MainActivity : AppCompatActivity() {
 
-    var shakeTurn = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, Fragment_Playing()).commit()
 
-        var sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        var shakeDetector = ShakeDetector(this)
-        shakeDetector.start(sensorManager)
-    }
-
-    override fun hearShake() {
-        shakeTurn ++
-        var ran = listOf<Int>()
-        for (x in 1..5) {
-            ran += (1..6).random()
+        btn_playing.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment, Fragment_Playing()).commit()
         }
-        txtView.setText(shakeTurn.toString()+ran)
-        setDiceImage(dice_no1,ran[0])
-        setDiceImage(dice_no2,ran[1])
-        setDiceImage(dice_no3,ran[2])
-        setDiceImage(dice_no4,ran[3])
-        setDiceImage(dice_no5,ran[4])
-    }
 
-    fun setDiceImage(imageView: ImageView, number: Int){
-        when (number) {
-            1 -> imageView.setImageResource(R.drawable.ic_dice_1)
-            2 -> imageView.setImageResource(R.drawable.ic_dice_2)
-            3 -> imageView.setImageResource(R.drawable.ic_dice_3)
-            4 -> imageView.setImageResource(R.drawable.ic_dice_4)
-            5 -> imageView.setImageResource(R.drawable.ic_dice_5)
-            else -> imageView.setImageResource(R.drawable.ic_dice_6)
+        btn_history.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment, Fragment_History()).commit()
+        }
+
+        btn_rules.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment, Fragment_Rules()).commit()
         }
     }
 
