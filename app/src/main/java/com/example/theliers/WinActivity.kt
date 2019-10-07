83bef9de515018ae0ed139b9fdf7e462c14f10f8
Thanh Le <nhatthanhh1993@gmail.com>
2019-10-07 23:58:57 +0300

    deleted black-dice and question-background

---
+++ app/src/main/java/com/example/theliers/WinActivity.kt
@@ -0,0 +1,41 @@
+package com.example.theliers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_lose.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_win)
+
+        button.setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+        }
+
+        saveWinResultToExternalStorage()
+    }
+
+    fun saveWinResultToExternalStorage() {
+        if( Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            var sharedPreference = SharedPreference(this)
+            val userName = sharedPreference.getUsername()
+            val opponentName = sharedPreference.getEnemyName()
+            val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val inputText = "[MatchResult:] ${timeStamp} : ${userName} WIN AGAINST ${opponentName}"
+            val filePath = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+            val file = File(filePath, "History.txt")
+            file.appendText(inputText)
+        } else {
+            Toast.makeText(this, "No dice saved", Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
