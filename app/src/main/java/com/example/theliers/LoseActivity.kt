1d35eeed6a06f84ac815a4d11827147c4cd77ab6
Thanh Le <nhatthanhh1993@gmail.com>
2019-10-07 23:52:20 +0300

    save match result to history

--- app/src/main/java/com/example/theliers/LoseActivity.kt
+++ app/src/main/java/com/example/theliers/LoseActivity.kt
@@ -3,7 +3,12 @@ package com.example.theliers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_lose.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LoseActivity : AppCompatActivity() {
 
@@ -13,6 +18,23 @@ class LoseActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             startActivity(Intent(this, MenuActivity::class.java))
+        }
+
+        saveLoseResultToExternalStorage()
+    }
+
+    fun saveLoseResultToExternalStorage() {
+        if( Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            var sharedPreference = SharedPreference(this)
+            val userName = sharedPreference.getUsername()
+            val opponentName = sharedPreference.getEnemyName()
+            val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val inputText = "[MatchResult:] ${timeStamp} : ${userName} LOSE AGAINST ${opponentName}"
+            val filePath = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+            val file = File(filePath, "History.txt")
+            file.appendText(inputText)
+        } else {
+            Toast.makeText(this, "No dice saved", Toast.LENGTH_LONG).show()
         }
     }
 }
